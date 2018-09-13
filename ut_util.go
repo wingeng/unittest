1d7aab2c0bd631c01080dc1a099831cf57bd9183
wing <wingeng@yahoo.com>
2018-09-13 09:26:59 -0400

    Add a skip-level api
    Added SkipExtraPush()/Pop() to allow the caller to add batched
    test routines that will point the error to the right location.
    
    Example:
    
    40: func myBatchTest(s) {
    41:    unittest.SkipExtraPush()
    42:    unittest.TEST_STR(s, "foo")
    43:    unittest.SkipExtraPop()
    44: }
    
    100: func TestMe(t *testing.T) {
    101:    myBatchTest("foo")
    102:    myBatchTest("a")
    103: }

--- ut_util.go
+++ ut_util.go
@@ -7,7 +7,13 @@ import (
 	"testing"
 )
 
+var (
+	skipExtra = 0
+)
+
 func test_error_msg(t *testing.T, skip int, msg string) {
+	skip += 2
+
 	_, file, line, ok := runtime.Caller(skip)
 	if !ok {
 		file = "???"
@@ -20,16 +26,37 @@ func test_error_msg(t *testing.T, skip int, msg string) {
 	t.Fail()
 }
 
+// Set this in case you wrap TEST, TEST_EQ inside
+// a wrapper that needs to be skipped
+// Example:
+//    func myTestWrapper (s string, exp string) {
+//       ut.SkipExtraPush()
+//       TEST_STR(s, exp)
+//       ut.SkipExtraPop()
+//    }
+//
+//    myTestWrapper("1", "2")  << will report error here
+//    myTestWrapper("2", "2")
+
+func SkipExtraPush() {
+	skipExtra += 1
+}
+func SkipExtraPop() {
+	if skipExtra > 0 {
+		skipExtra -= 1
+	}
+}
+
 func TEST(t *testing.T, b bool) {
 	if !b {
-		test_error_msg(t, 2, "Failed Test")
+		test_error_msg(t, skipExtra, "Failed Test")
 	}
 }
 
 func TEST_EQ(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		msg := fmt.Sprint(a, " not equal to ", b, "\n")
-		test_error_msg(t, 2, msg)
+		test_error_msg(t, skipExtra, msg)
 	}
 }
 
@@ -39,6 +66,6 @@ func TEST_STR(t *testing.T, s1 string, s2 string) {
 	if s1 != s2 {
 		msg := fmt.Sprintf("\n\nstrings don't match\ns1: '%s'\n\ns2: '%s'",
 			s1, s2)
-		test_error_msg(t, 2, msg)
+		test_error_msg(t, skipExtra, msg)
 	}
 }
